43551139026e3d8ea3332952a0a9105688608adb
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-12-17 12:18:03 -0500

    Added background image

--- petres.js
+++ petres.js
@@ -2,12 +2,13 @@ function centerCanvas() {
 	var x = (windowWidth - width) / 2;
 	var y = (windowHeight - height) / 2;
 	cnv.position(x, y);
+	background(0);
 }
 
 function setup() {
-  
-	cnv = createCanvas(windowWidth, windowWidth*0.56);
+	cnv = createCanvas(windowHeight-100, windowHeight*0.5);
 	centerCanvas();
+	background(0);
 }
 
 function draw() {
@@ -16,4 +17,5 @@ function draw() {
 
 function windowResized() {
 	centerCanvas();
+	background(0);
 }
