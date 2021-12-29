1fde8af5370f98053f4b534303b45f0bbe74e440
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-12-28 23:59:29 -0500

    added ball class

--- petres.js
+++ petres.js
@@ -11,33 +11,48 @@ function centerCanvas() {
 	background(0);
 }
 
-function preload(){
-	petres = loadImage('./petres.jpg')
+function preload() {
+	petres = loadImage('./petres.jpg');
 }
 
 function setup() {
 	centerCanvas();
 }
 
+let 
+
 function draw() {
 	let w = width;
 	let h = height;
 
+	/* Draw net */
 	stroke(255);
 	for (let i = 0; i < h; i += 30) line(w / 2, i, w / 2, i + 10);
 
+	/* Draw Paddles*/
 	let paddleWidth = w * 0.005;
 	let paddleHeight = h * 0.15;
 	let paddleLX = w - w * 0.98;
 	let paddleRX = w - w * 0.02 - paddleWidth;
-	let paddleLY = h / 2 - paddleHeight/2;
-	let paddleRY = h / 2 - paddleHeight/2;
+	let paddleLY = h / 2 - paddleHeight / 2;
+	let paddleRY = h / 2 - paddleHeight / 2;
 
 	rect(paddleLX, paddleLY, paddleWidth, paddleHeight);
 	rect(paddleRX, paddleRY, paddleWidth, paddleHeight);
-	image(petres, w/2-50, h/2-50, 100, 100)
+	
+	
+
+	//image(petres, w / 2 - 25, h / 2 - 25, w / 25, w / 25);
+
 }
 
 function windowResized() {
 	centerCanvas();
+}
+
+class Plasyer {
+	constructor() {
+		this.height = width * 0.005;
+		this.width = height * 0.15;
+	}
 }
---
+++ player.js
@@ -0,0 +1,6 @@
+function Player(){
+  this.width = width * 0.005;
+	this.height = height * 0.15;
+
+  this.pos = createVector(this.width, )
+}
