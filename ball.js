c31b51361978e6830e7c34a93f8baa80ddb8b488
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-12-30 23:58:18 -0500

    added Ball and Player class

--- ball.js
+++
@@ -1,6 +0,0 @@
-function Ball(){
-  this.pos = createVector(width/2, height/2);
-  this.radius = 50;
-  this.maxSpeed = createVector(20,15);
-  this.acc = p5.Vector.random2D();
-}
--- petres.js
+++ petres.js
@@ -1,3 +1,7 @@
+let ball;
+let player1;
+let player2;
+
 function centerCanvas() {
 	let winWidth = windowHeight - 100;
 	let winHeight = windowHeight * 0.5;
@@ -8,6 +12,13 @@ function centerCanvas() {
 	}
 	canvas = createCanvas(winWidth, winHeight);
 	canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+
+	let w = width;
+	let h = height;
+	ball = new Ball();
+	player1 = new Player(w - w * 0.98, h / 2 - (h * 0.15) / 2);
+	player2 = new Player(w - w * 0.02 - w * 0.005, h / 2 - (h * 0.15) / 2);
+
 	background(0);
 }
 
@@ -19,9 +30,8 @@ function setup() {
 	centerCanvas();
 }
 
-let 
-
 function draw() {
+	background(0);
 	let w = width;
 	let h = height;
 
@@ -29,30 +39,65 @@ function draw() {
 	stroke(255);
 	for (let i = 0; i < h; i += 30) line(w / 2, i, w / 2, i + 10);
 
-	/* Draw Paddles*/
-	let paddleWidth = w * 0.005;
-	let paddleHeight = h * 0.15;
-	let paddleLX = w - w * 0.98;
-	let paddleRX = w - w * 0.02 - paddleWidth;
-	let paddleLY = h / 2 - paddleHeight / 2;
-	let paddleRY = h / 2 - paddleHeight / 2;
-
-	rect(paddleLX, paddleLY, paddleWidth, paddleHeight);
-	rect(paddleRX, paddleRY, paddleWidth, paddleHeight);
-	
-	
-
-	//image(petres, w / 2 - 25, h / 2 - 25, w / 25, w / 25);
-
+	ball.show();
+	player1.show();
+	player2.show();
+	ball.move();
 }
 
 function windowResized() {
 	centerCanvas();
 }
 
-class Plasyer {
-	constructor() {
-		this.height = width * 0.005;
-		this.width = height * 0.15;
+function keyPressed() {
+	if ( keyCode == UP_ARROW) {
+		console.log('up')
+		player1.up();
 	}
 }
+
+class Player {
+	constructor(x, y) {
+		this.w = width * 0.005;
+		this.h = height * 0.15;
+		this.x = x;
+		this.y = y;
+		this.acc = createVector(0,0);
+		this.spd = 10;
+	}
+
+	show() {
+		rect(this.x, this.y, this.w, this.h);
+	}
+
+	up() {
+		this.acc -= this.spd;
+	}
+}
+
+class Ball {
+	constructor() {
+		this.pos = createVector(width / 2, height / 2);
+		this.r = 50;
+		this.maxSpeed = createVector(20, 15);
+		this.acc = p5.Vector.random2D();
+	}
+
+	show() {
+		circle(this.pos.x, this.pos.y, this.r);
+	}
+
+	move() {
+		this.pos.x += 5 * this.acc.x;
+	}
+}
+
+/**
+ * 	 Draw Paddles
+	let paddleWidth = w * 0.005;
+	let paddleHeight = h * 0.15;
+	let paddleLX = w - w * 0.98;
+	let paddleRX = w - w * 0.02 - paddleWidth;
+	let paddleLY = h / 2 - paddleHeight / 2;
+	let paddleRY = h / 2 - paddleHeight / 2;
+ */
